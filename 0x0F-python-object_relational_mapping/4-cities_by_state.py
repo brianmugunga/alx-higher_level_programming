21916b3553b3e125569ff6e3f1883d040ab40146
brianmugunga <104896959+brianmugunga@users.noreply.github.com>
2022-12-03 22:32:50 +0300

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,21 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """
-Lists all cities from the database using foreing key
+script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
 """
-
-import MySQLdb
 from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    query = "SELECT cities.id, cities.name, states.name\
-             FROM cities JOIN states\
-             WHERE cities.state_id = states.id"
-    cursor = db.cursor()
-    cursor.execute(query)
-    for state in cursor.fetchall():
-        print(state)
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                FROM cities
+                LEFT JOIN states ON cities.state_id = states.id
+                ORDER BY cities.id ASC""")
+    cities = cur.fetchall()
+    for city in cities:
+        print(city)
+    cur.close()
     db.close()
